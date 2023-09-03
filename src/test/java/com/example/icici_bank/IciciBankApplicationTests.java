fc646f098be29b21df64f8bb34c83acff0f1e281
abhijeet <wankhedeabhijeet242@gmail.com>
2023-09-03 12:18:38 +0530

    first commit

---
+++ src/test/java/com/example/icici_bank/IciciBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icici_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IciciBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
